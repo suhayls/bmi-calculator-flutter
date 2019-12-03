983f829dc4b220db2bb783fbde564601c49ab218
Suhayl <suhayls@gmail.com>
2019-12-03 17:39:49 +0800

    Complete BMI App. Gender & Age input is not utilized anywhere in app and purely cosmetic.

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -6,6 +6,7 @@ import 'package:bmi_calculator/constants.dart';
 import 'results_page.dart';
 import '../components/bottom_button.dart';
 import '../components/round_icon_button.dart';
+import 'package:bmi_calculator/calculator_brain.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -222,10 +223,17 @@ class _InputPageState extends State<InputPage> {
           BottomButton(
             buttonTitle: 'CALCULATE',
             onTap: () {
+              CalculatorBrain calc =
+                  CalculatorBrain(height: height, weight: weight);
+
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => ResultsPage(),
+                  builder: (context) => ResultsPage(
+                    bmiResult: calc.calculateBMI(),
+                    resultText: calc.getResult(),
+                    interpretation: calc.getInterpretation(),
+                  ),
                 ),
               );
             },
--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -4,6 +4,15 @@ import '../components/reusable_card.dart';
 import '../components/bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.bmiResult,
+      @required this.resultText,
+      @required this.interpretation});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,15 +42,15 @@ class ResultsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'OVERWEIGHT',
+                    resultText.toUpperCase(),
                     style: kResultStyleText,
                   ),
                   Text(
-                    '25.5',
+                    bmiResult,
                     style: kBMITextStyle,
                   ),
                   Text(
-                    'Go to the gym',
+                    interpretation,
                     style: kBodyTextStyle,
                   ),
                 ],
